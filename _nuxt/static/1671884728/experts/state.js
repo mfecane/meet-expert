9d2500e21a567b15fa9ec7b1c3010f7f5413686a
Aleksei Aliapkin <a.alyapkin@tais.ru>
2022-12-24 15:25:31 +0300

    Updates

---
+++ _nuxt/static/1671884728/experts/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmeet-expert\u002F_nuxt\u002Fstatic\u002F1671884728",layout:"default",error:a,serverRendered:true,routePath:"\u002Fexperts",config:{_app:{basePath:"\u002Fmeet-expert\u002F",assetsPath:"\u002Fmeet-expert\u002F_nuxt\u002F",cdnURL:a}}}}(null));
